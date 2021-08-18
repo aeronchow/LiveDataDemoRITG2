4c88e0f3ecba98e08b22441bafebd29e5ac10125
aeronchow <68983153+aeronchow@users.noreply.github.com>
2021-08-18 14:10:15 +0800

    Initial commit

---
+++ app/src/main/java/my/edu/tarc/livedatademoritg2/MainActivity.kt
@@ -0,0 +1,48 @@
+package my.edu.tarc.livedatademoritg2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import my.edu.tarc.livedatademoritg2.databinding.ActivityMainBinding
+import my.edu.tarc.livedatademoritg2.models.AccountModel
+
+class MainActivity : AppCompatActivity() {
+    private val myAccount = AccountModel("A1234566","John", 500.00)
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        binding =  DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.myAccount = myAccount
+        binding.lifecycleOwner = this
+        //display()
+
+        //myAccount.balance.observe(this, Observer { newBalance -> binding.tvBalance.text = newBalance.toString() })
+
+
+        binding.btnDeposit.setOnClickListener(){
+            val amount:Double  = binding.tfAmount.text.toString().toDouble()
+
+            myAccount.deposit(amount)
+            //binding.tvBalance.text = myAccount.balance.toString()
+
+        }
+
+        binding.btnWitdraw.setOnClickListener(){
+            val amount:Double  = binding.tfAmount.text.toString().toDouble()
+            myAccount.withdraw(amount)
+            //binding.tvBalance.text = myAccount.balance.toString()
+        }
+    }
+
+//    fun display(){
+//
+//        binding.tvAccNo.text = myAccount.accNo
+//        binding.tvName.text = myAccount.owner
+//        binding.tvBalance.text = myAccount.balance.value.toString()
+//
+//    }
+}
